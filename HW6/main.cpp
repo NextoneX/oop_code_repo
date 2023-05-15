87a97b60dd870bc3aec3fd6e8a4122513833980b
NextoneX <2535049640@qq.com>
2023-05-15 10:30:06 +0800

    update hw6(without comment)

--- HW6/main.cpp
+++ HW6/main.cpp
@@ -1,8 +1,37 @@
 // main.cpp
 // header
 #include "vector.h"
+#include <iostream>
+using namespace std;
 //main
 int main()
 {   
-    
+    //!todo 
+    //some test
+    Vector<int> v1(2);
+    v1.push_back(1);
+    v1.push_back(2);
+    v1.push_back(3);
+    cout << v1[1] << " " << v1.at(1) << " " << v1.size() << " " << v1.capacity() << " " << v1.empty() << endl;
+    v1.clear();
+    cout << v1[1] << " " << v1.size() << " " << v1.empty() << endl;
+    try
+    {
+        v1.at(1);
+    }
+    catch(const std::out_of_range& e)
+    {
+        std::cerr << e.what() << '\n';
+    }
+    try
+    {
+        v1.at(-1);
+    }
+    catch(const std::out_of_range& e)
+    {
+        std::cerr << e.what() << '\n';
+    }
+    Vector<int> v2 = v1;
+    for(int i = 0; i<30; ++i) v2.push_back(1);
+    cout << v2.size() << " " << v2.capacity() << endl;
 }
--- HW6/vector.h
+++ HW6/vector.h
@@ -1,8 +1,7 @@
 // fraction.h
 #ifndef _VECTOR_
 #define _VECTOR_
-#include <exception>
-using namespace std;
+#include <stdexcept>
 
 template <class T>
 class Vector {
@@ -15,6 +14,7 @@ public:
   T& at(int index);              // accesses the specified element, throws an exception of
                                  // type 'std::out_of_range' when index <0 or >=m_nSize
   int size() const;              // return the size of the container
+  int capacity() const;          // return the capacity of the container
   void push_back(const T& x);    // adds an element to the end 
   void clear();                  // clears the contents
   bool empty() const;            // checks whether the container is empty 
@@ -27,4 +27,83 @@ private:
                                  // allocated storage
 };
 
+template <class T>
+Vector<T>::Vector()
+    : m_pElements(nullptr),
+      m_nSize(0),
+      m_nCapacity(10) {
+    m_pElements = new T[m_nCapacity];
+}
+
+template <class T>
+Vector<T>::Vector(int size)
+    : m_pElements(nullptr),
+      m_nSize(0),
+      m_nCapacity(size) {
+    m_pElements = new T[m_nCapacity];
+}
+
+template <class T>
+Vector<T>::Vector(const Vector& r)
+    : m_pElements(nullptr),
+      m_nSize(r.m_nSize),
+      m_nCapacity(r.m_nCapacity) {
+    m_pElements = new T[m_nCapacity];
+    for(int i = 0; i < m_nSize; ++i)
+        m_pElements[i] = r.m_pElements[i];
+}
+
+template <class T>
+Vector<T>::~Vector() {
+    delete [] m_pElements;
+}
+
+template <class T>
+T& Vector<T>::operator[](int index) {
+    return m_pElements[index];
+}
+
+template <class T>
+T& Vector<T>::at(int index) {
+    if(index < 0 || index >= m_nSize)
+        throw std::out_of_range("out_of_range error");
+    return m_pElements[index];
+}
+
+template <class T>
+int Vector<T>::size() const {
+    return m_nSize;
+}
+
+template <class T>
+int Vector<T>::capacity() const {
+    return m_nCapacity;
+}
+
+template <class T>
+void Vector<T>::push_back(const T& x) {
+    while(m_nSize + 1 > m_nCapacity) 
+        inflate();
+    m_pElements[m_nSize++] = x;
+}
+
+template <class T>
+void Vector<T>::clear() {
+    m_nSize = 0;
+}
+
+template <class T>
+bool Vector<T>::empty() const {
+    return m_nSize == 0;
+}
+
+template <class T>
+void Vector<T>::inflate() {
+    m_nCapacity *= 2;
+    T* temp = new T[m_nCapacity];
+    for(int i = 0; i < m_nSize; ++i)
+        temp[i] = m_pElements[i];
+    delete [] m_pElements;
+    m_pElements = temp;
+}
 #endif
